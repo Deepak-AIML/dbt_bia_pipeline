97e04aaff17dec4b94085c12eb766be98d826784
Deepak Kapoor <deepakkapoor16@gmail.com>
2025-02-28 20:09:28 +0530

    mod path

---
+++ bia_pipeline/macros/macro_channel_staging.sql
@@ -0,0 +1,16 @@
+{% macro macro_channel_staging(table_name) %}
+
+{% set table_ref = "`" ~ 'dbt-pipeline-415806' ~ "." ~ 'campaign' ~ table_name%}
+
+with df as (
+select
+  date(date) as date,
+  channel,
+  campaign_name,
+  event_name,
+  value
+  FROM {{ table_ref }})
+
+select * from df
+
+{% endmacro %}
